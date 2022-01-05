389256b75ee698bd70ec9e2b96cb741361669dce
JJiGolem <lorsanov2000@gmail.com>
2022-01-05 23:04:42 +0300

    Добавлена настройка урона для группы оружий

--- client_packages/src/weapons/damageHandler/index.js
+++ client_packages/src/weapons/damageHandler/index.js
@@ -1,39 +1,41 @@
-const {
-	ignoreWeapons,
-	damageWeapons
-} = require('./src/weapons/damageHandler/settings.js');
+const { ignoreWeapons, damageWeapons, damageWeaponGroups } = require('./src/weapons/damageHandler/settings.js');
 
-// Дефолтные проценты, которые мы будем отнимать от входящего урона.
 const defaultPercent = {
-	"max": 85,
-	"min": 60
-};
+	max: 85,
+	min: 60
+}
 
-// Событие принятия входящего урона, нанесенного нам
-mp._events.add('incomingDamage', (sourceEntity, sourcePlayer, targetEntity, weapon, boneIndex, damage) => {
-	if (targetEntity.type === 'player' && sourcePlayer && !(weapon in ignoreWeapons)) {
+const randomInt = (min, max) => Math.random() * (max - min) + min;
 
-		if (global.adminGodMode) return true;
+mp._events.add("incomingDamage", (sourceEntity, sourcePlayer, targetEntity, weapon, boneIndex, damage) => {
+	if (targetEntity.type === "player" && sourcePlayer && !(weapon in ignoreWeapons)) {
+		if (global.adminGodMode)  {
+			return true;
+		}
 
 		let max = defaultPercent.max;
-		let min = defaultPercent.min;
+		let min = defaultPercent.max;
 
+		const weaponGroupHash = mp.game.weapon.getWeapontypeGroup(weapon);
 		if (weapon in damageWeapons) {
 			max = damageWeapons[weapon].max;
 			min = damageWeapons[weapon].min;
 		}
+		else if (weaponGroupHash in damageWeaponGroups) {
+			max = damageWeaponGroups[weaponGroupHash].max;
+			min = damageWeaponGroups[weaponGroupHash].min;
+		}
 
-		let percent = randomInt(min, max) / 100;
-		let cDamage = damage - (damage * percent);
+		const percent = randomInt(min, max) / 100;
+		let customDamage = damage - (damage * percent);
 
-		if (boneIndex === 20)
-			cDamage = cDamage / 10;
-
-		targetEntity.applyDamageTo(parseInt(cDamage), true);
-
+		if (boneIndex === 20) {
+			customDamage /= 10;
+		}
+		
 		const currentHealth = targetEntity.getHealth();
 		if (currentHealth > 0) {
 			return true;
 		}
 	}
-});
+})
--- client_packages/src/weapons/damageHandler/settings.js
+++ client_packages/src/weapons/damageHandler/settings.js
@@ -1,66 +1,63 @@
-/**
- * Список игнорируемых оружий
- * Их урон не будет снижаться или повышаться
- */
-const ignoreWeapons = {
-	911657153: "Stun Gun",
-}
-
-/**
- * Настройки максимального и минимального процента
- * Проходимого урона от данных оружий по игроку
- */
-const damageWeapons = {
-	// Пистолеты
-	// хеш оружия
-	3249783761: {
-		// Название оружия, это для нас, чтобы в будущем смогли быстро найти нужное нам оружие.
-		"name": "Heavy Revolver",
-		// Максимальный процент.
-		"max": 90,
-		// Минимальный процент.
-		"min": 80,
-		// Эти проценты нужны для функции рандома.
-	},
-	// Пистолет пулеметы
-	324215364: {
-		"name": "Micro SMG",
-		"max": 80,
-		"min": 50
-	},
-	// Пулеметы
-	2144741730: {
-		"name": "Combat MG",
-		"max": 65,
-		"min": 35
-	},
-	// Карабины
-	3220176749: {
-		"name": "Assault Rifle",
-		"max": 70,
-		"min": 45
-	},
-	// Дробовики
-	487013001: {
-		"name": "Pump Shotgun",
-		"max": 80,
-		"min": 30,
-	},
-	// Снайперы
-	100416529: {
-		"name": "Sniper Rifle",
-		"max": 80,
-		"min": 50,
-	},
-	// Холодное оружие
-	3441901897: {
-		"name": "Battle Axe",
-		"max": 50,
-		"min": 40
-	}
-}
-
 exports = {
-	ignoreWeapons: ignoreWeapons,
-	damageWeapons: damageWeapons
+	ignoreWeapons: {
+		911657153: "Stun Gun",
+	},
+	damageWeapons: {
+		3249783761: { // weaponHash
+			max: 90,
+			min: 85
+		},
+	},
+	damageWeaponGroups: {
+		2685387236: {	// weaponGroupHash
+			name: "melee",
+			max: 80,
+			min: 40
+		},
+		416676503: {
+			name: "handguns",
+			max: 80,
+			min: 40
+		},
+		3337201093: {
+			name: "submachine gun",
+			max: 80,
+			min: 40
+		},
+		860033945: {
+			name: "shotgun",
+			max: 80,
+			min: 40
+		},
+		970310034: {
+			name: "Assault Rifle",
+			max: 80,
+			min: 40
+		},
+		1159398588: {
+			name: "Light Machine Gun",
+			max: 80,
+			min: 40
+		},
+		3082541095: {
+			name: "Sniper",
+			max: 80,
+			min: 40
+		},
+		2725924767: {
+			name: "Heavy Weapon",
+			max: 80,
+			min: 40
+		},
+		1548507267: {
+			name: "Throwables",
+			max: 80,
+			min: 40
+		},
+		4257178988: {
+			name: "Misc",
+			max: 80,
+			min: 40
+		}
+	}
 }
